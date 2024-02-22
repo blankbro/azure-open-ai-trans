70646f911b77957910e9ceed77c77a7a33d8d2f2
lizexin <zexin-li@outlook.com>
2024-02-22 16:53:54 +0800

    简化逻辑(部署测试)
    1. 优化启动命令 control.sh

--- control.sh
+++ control.sh
@@ -27,8 +27,17 @@ Stop(){
 }
 
 Start(){
+    pid=$(ps -ef | grep "${name}" | grep -v grep | grep -v kill | awk '{print $2}')
+    if [ -n "${pid}" ]; then
+        echo "进程已启动"
+        exit 0
+    fi
+
     echo "开始启动"
-    nohup python src/app.py --name=${name} 2>&1>/dev/null &
+    # echo "nohup python src/app.py --name=${name} 2>&1 &" # 这种方式在 ubuntu 不好使，在 mac 可以
+    echo "python src/app.py --name=${name} &>/dev/null &"
+    python src/app.py --name=${name} &>/dev/null &
+
     for ((i=0; i<10; ++i)) do
         sleep 1
         pid=$(ps -ef | grep "${name}" | grep -v grep | grep -v kill | awk '{print $2}')
